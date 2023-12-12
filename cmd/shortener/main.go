0120fcc03984a5487dca5d965b11cd17a6bb7d7f
matvenick <46794223+matvenick@users.noreply.github.com>
2023-12-13 02:37:39 +0300

    Добавлен пакет flag

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"go-url-shortener/internal/app/config" // Замените на ваш путь к пакету config
+)
+
+func main() {
+	cfg := config.InitConfig()
+
+	// Дальнейшая логика вашего приложения, использующая cfg
+	fmt.Println("Server address:", cfg.Address)
+	fmt.Println("Base URL:", cfg.BaseURL)
+	fmt.Println("Environment:", cfg.Environment)
+
+	// Запуск сервера и т.д.
+}
