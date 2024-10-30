28f99585c62343b3dd430839ce94cd9c3157ede8
matvenick <matvenick@gmail.com>
2024-10-30 16:14:57 +0700

    итерация 5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,6 +10,12 @@ import (
 	"path/filepath"
 )
 
+type flagsData struct {
+	serverAddress *string
+	baseURL       *string
+	filePath      *string
+}
+
 func main() {
 	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
@@ -19,37 +25,48 @@ func main() {
 
 	log.SetOutput(f)
 
-	serverAddress := flagOrEnv("a", "SERVER_ADDRESS", "localhost:8080", "HTTP-сервер адрес")
-	baseURL := flagOrEnv("b", "BASE_URL", "http://localhost:8080", "Базовый адрес для сокращения URL")
-
 	// Используем кроссплатформенный путь для временного файла
 	defaultFilePath := filepath.Join(os.TempDir(), "short-url-db.json")
-	filePath := flagOrEnv("f", "FILE_STORAGE_PATH", defaultFilePath, "Путь к файлу для сохранения данных")
 
+	var flags flagsData
+	flags.serverAddress = flag.String("a", "localhost:8080", "HTTP-сервер адрес")
+	flags.baseURL = flag.String("b", "http://localhost:8080", "Базовый адрес для сокращения URL")
+	flags.filePath = flag.String("f", defaultFilePath, "Путь к файлу для сохранения данных")
 	flag.Parse()
 
-	if _, er := os.Stat(*filePath); errors.Is(er, os.ErrNotExist) {
-		er = os.WriteFile(*filePath, []byte("[]"), 0644)
+	cfg := &config.Config{
+		ServerAddress: *flags.serverAddress,
+		BaseURL:       *flags.baseURL,
+		UrlsPath:      *flags.filePath,
+	}
+	if value, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+		cfg.ServerAddress = value
+	}
+	if value, ok := os.LookupEnv("BASE_URL"); ok {
+		cfg.BaseURL = value
+	}
+	if value, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
+		cfg.UrlsPath = value
+	}
+
+	if _, er := os.Stat(cfg.UrlsPath); errors.Is(er, os.ErrNotExist) {
+		er = os.WriteFile(cfg.UrlsPath, []byte("[]"), 0644)
 		if er != nil {
 			log.Fatalf("Failed to create file for urls: %v", er)
 			return
 		}
 	}
 
-	if *filePath == defaultFilePath {
+	if cfg.UrlsPath == defaultFilePath {
 		defer os.Remove(defaultFilePath)
 	}
 
-	srv, err := server.NewServer(&config.Config{
-		ServerAddress: *serverAddress,
-		BaseURL:       *baseURL,
-		UrlsPath:      *filePath,
-	})
+	srv, err := server.NewServer(cfg)
 	if err != nil {
 		log.Fatalf("Failed to create shortener: %v", err)
 		return
 	}
-	srv.Start(*serverAddress)
+	srv.Start(cfg.ServerAddress)
 }
 
 func flagOrEnv(flagName, envVarName, fallbackValue, description string) *string {
